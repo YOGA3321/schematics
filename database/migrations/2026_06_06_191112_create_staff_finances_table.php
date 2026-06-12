9bb2fd2ba5a1aaa1d23218989de94eefe7b96436
YOGA3321 <ageng.prayoga321@gmail.com>
2026-06-12 19:49:34 +0700

    s

--- database/migrations/2026_06_06_191112_create_staff_finances_table.php
+++ database/migrations/2026_06_06_191112_create_staff_finances_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
             $table->string('nama_lengkap', 50);
             $table->char('jenis_kelamin', 1);
             $table->string('nomor_telepon', 15);
-            $table->string('password')->default('$2y$12$N9qo8uLOqp.9wt129a0Uee/s3.eI4O74Gex/mrrB1Y.2Zf9fT51.a'); // Default: password123
+            $table->string('password')->default('$2y$12$BJTBhakHgrB/pXJ0Z6enWOkXGkKAlXOeR7NsQ0OY.OCIPw.O1d83u'); // Default: password123
             $table->timestamps();
         });
     }
